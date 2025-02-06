39a27921b95914b2a33ee2d2f8b2f6277c19cff6
Caekebeke <liano.caekebeke@sap.com>
2025-02-06 13:50:12 +0100

    test

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import fetch_emails
 import generate_rss
 import subprocess
 import time
+import os
+
+def push_to_github():
+    os.system('git add emails_rss.rss')
+    os.system('git commit -m "Update RSS feed"')
+    os.system('git push origin main')
 
 if __name__ == "__main__":
     while True:
@@ -11,6 +17,8 @@ if __name__ == "__main__":
 
         with open('emails_rss.rss', 'w') as file:
             file.write(rss_feed.decode('utf-8'))
+        
+        push_to_github()
         
         # Wait for 1 minute before running again
         time.sleep(60)
