db87ef1ba74419940ad246daf67bec6fa008a814
Caekebeke <liano.caekebeke@sap.com>
2025-02-06 10:56:37 +0100

    test

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import fetch_emails
 import generate_rss
 import subprocess
+import time
 
 if __name__ == "__main__":
-    email_addresses = fetch_emails.get_emails()
-    
-    rss_feed = generate_rss.generate_rss(email_addresses)
+    while True:
+        email_addresses = fetch_emails.get_emails()
+        
+        rss_feed = generate_rss.generate_rss(email_addresses)
 
-    with open ('emails_rss.rss', 'w') as file:
-        file.write(rss_feed.decode('utf-8'))
+        with open('emails_rss.rss', 'w') as file:
+            file.write(rss_feed.decode('utf-8'))
+        
+        # Wait for 1 minute before running again
+        time.sleep(60)
